378f582eee280985a2723af569fc52d80f2530f9
ruijpedro <rjpedro@gmail.com>
2026-06-04 20:15:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 const isAndroid = process.env.BUILD_TARGET === 'android'
-export default defineConfig({base:isAndroid?'./':'/Cav-Academy-Player/',plugins:[react()],build:{outDir:'dist',emptyOutDir:true}})
+
+export default defineConfig({
+  base: isAndroid ? './' : '/Cavadas-Academy_Player/',
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
